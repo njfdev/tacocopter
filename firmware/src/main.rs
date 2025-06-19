13f7568f25778cd07a9b33284ecd3d654b7f356e
Nicholas Fasching <91689117+njfdev@users.noreply.github.com>
2025-06-19 10:09:16 -0400

    Add Preliminary Altitude Holding Logic

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -135,6 +135,8 @@ static GPS_SIGNAL: Watch<CriticalSectionRawMutex, GPSPayload, 2> = Watch::new();
 static ULTRASONIC_WATCH: Watch<CriticalSectionRawMutex, f32, 1> = Watch::new();
 // in format of (pressure in kPa, temperature in kelvin, estimated altitude in m)
 static BMP390_WATCH: Watch<CriticalSectionRawMutex, (f32, f32, f32), 1> = Watch::new();
+// processed in position hold control loop, for use in the control loop under position hold mode
+static CURRENT_ALTITUDE: Watch<CriticalSectionRawMutex, f32, 1> = Watch::new();
 // In celsius
 static TEMPERATURE: Signal<ThreadModeRawMutex, f32> = Signal::new();
 
@@ -532,6 +534,8 @@ async fn position_hold_loop() {
     //     Matrix1::new(R_GPS),
     // );
 
+    let altitude_sender = CURRENT_ALTITUDE.sender();
+
     loop {
         let dt = (last_update.elapsed().as_micros() as f32) / 1_000_000.0;
         last_update = Instant::now();
@@ -596,6 +600,8 @@ async fn position_hold_loop() {
                     (ultrasonic_payload - ULTRASONIC_HEIGHT_ABOVE_BOTTOM).max(0.0);
             }
         }
+
+        altitude_sender.send(barometer_height);
 
         if (last_print.elapsed().as_millis() > 100) {
             if gps_locked_sats > 0 {
@@ -738,7 +744,7 @@ With motor 1 and 4 CCW and motor 2 and 3 CW
 const MAX_ACRO_RATE: f32 = 200.0; // What is the target rotation rate at full throttle input?
 #[embassy_executor::task]
 async fn control_loop() {
-    // pid
+    // rotation pid
     let mut pid_yaw = Pid::new(0.0, 0.25);
     pid_yaw.p(0.008, 0.5);
     pid_yaw.i(0.0001, 0.1);
@@ -752,8 +758,19 @@ async fn control_loop() {
     pid_pitch.i(0.000014, 0.1);
     pid_pitch.d(0.00013, 0.1);
 
+    // vertical pid
+    let mut pid_altitude = Pid::new(0.0, 7.5); // up to 7.5 m/s corrections
+    pid_altitude.p(5.0, 7.5);
+    pid_altitude.i(0.0, 7.5);
+    pid_altitude.d(0.8, 7.5);
+    let mut pid_vs = Pid::new(0.0, 0.5);
+    pid_vs.p(0.05, 0.4);
+    pid_vs.i(0.0002, 0.1);
+    pid_vs.d(0.0, 0.4);
+
     // elrs controls
     let mut armed = false;
+    let mut position_hold = false;
     let mut throttle_input = 0.0;
     let mut yaw_input = 0.0;
     let mut roll_input = 0.0;
@@ -763,6 +780,13 @@ async fn control_loop() {
     let mut rate_errors = (0.0, 0.0, 0.0);
     let mut imu_values = (0.0, 0.0, 0.0);
     let mut imu_rates = (0.0, 0.0, 0.0);
+
+    // altitude stuff
+    let mut altitude_receiver = CURRENT_ALTITUDE.receiver().unwrap();
+    let mut current_altitude = None;
+    let mut last_altitude = None;
+    let mut target_altitude = 0.0;
+    let mut current_vertical_speed = 0.0;
 
     let mut since_last_loop = Instant::now();
     let mut since_last_elrs_update = Instant::now();
@@ -776,6 +800,20 @@ async fn control_loop() {
         .await;
         let dt = (since_last_loop.elapsed().as_micros() as f32) / 1_000_000.0;
         since_last_loop = Instant::now();
+
+        let altitude_recv = altitude_receiver.try_changed();
+        if altitude_recv.is_some() && (current_altitude.is_some() || altitude_recv.unwrap() != 0.0)
+        {
+            if current_altitude.is_none() {
+                target_altitude = altitude_recv.unwrap();
+            }
+            current_altitude = altitude_recv;
+            current_vertical_speed = if last_altitude.is_some() {
+                (current_altitude.unwrap() - last_altitude.unwrap()) * dt
+            } else {
+                0.0
+            };
+        }
 
         let imu_recv = imu_reciever.try_get();
         if imu_recv.is_some() {
@@ -809,6 +847,15 @@ async fn control_loop() {
                 // rate_errors = imu_values;
             }
             armed = new_armed;
+            let new_position_hold = elrs_input_to_percent(chnls[7], None) > 0.5;
+            if position_hold == false && new_position_hold == true {
+                pid_altitude.reset_integral_term();
+                pid_vs.reset_integral_term();
+                if current_altitude.is_some() {
+                    target_altitude = current_altitude.unwrap();
+                }
+            }
+            position_hold = new_position_hold;
             throttle_input = elrs_input_to_percent(chnls[2], None);
             yaw_input = elrs_input_to_percent(chnls[0], Some(0.01)) * MAX_ACRO_RATE;
             roll_input = elrs_input_to_percent(chnls[3], Some(0.01)) * MAX_ACRO_RATE;
@@ -819,19 +866,42 @@ async fn control_loop() {
             since_last_elrs_update = Instant::now();
         }
 
+        // tc_println!("Position hold? {}", position_hold);
+
         // calc pid
         let pid_pitch_output = pid_pitch.next_control_output(rate_errors.0).output;
         let pid_roll_output = pid_roll.next_control_output(rate_errors.1).output;
         let pid_yaw_output = pid_yaw.next_control_output(rate_errors.2).output;
 
-        let t1 =
-            (throttle_input + pid_pitch_output + pid_roll_output - pid_yaw_output).clamp(0.0, 1.0);
-        let t2 =
-            (throttle_input + pid_pitch_output - pid_roll_output + pid_yaw_output).clamp(0.0, 1.0);
-        let t3 =
-            (throttle_input - pid_pitch_output + pid_roll_output + pid_yaw_output).clamp(0.0, 1.0);
-        let t4 =
-            (throttle_input - pid_pitch_output - pid_roll_output - pid_yaw_output).clamp(0.0, 1.0);
+        let mut pid_vs_output = 0.0;
+        let should_use_position_hold =
+            position_hold && current_altitude.is_some() && throttle_input > 0.3;
+        if should_use_position_hold {
+            let pid_altitude_output = pid_altitude
+                .next_control_output(current_altitude.unwrap() - target_altitude)
+                .output;
+            pid_vs_output = pid_vs
+                .next_control_output(current_vertical_speed - pid_altitude_output)
+                .output;
+            tc_println!("PID Alt: {}", pid_altitude_output);
+            tc_println!("PID Output: {}", pid_vs_output);
+        }
+        // tc_println!("Current: {}", current_altitude);
+
+        let current_throttle_value = if should_use_position_hold {
+            pid_vs_output + 0.5
+        } else {
+            throttle_input
+        };
+
+        let t1 = (current_throttle_value + pid_pitch_output + pid_roll_output - pid_yaw_output)
+            .clamp(0.0, 1.0);
+        let t2 = (current_throttle_value + pid_pitch_output - pid_roll_output + pid_yaw_output)
+            .clamp(0.0, 1.0);
+        let t3 = (current_throttle_value - pid_pitch_output + pid_roll_output + pid_yaw_output)
+            .clamp(0.0, 1.0);
+        let t4 = (current_throttle_value - pid_pitch_output - pid_roll_output - pid_yaw_output)
+            .clamp(0.0, 1.0);
 
         // TODO: implement return to home like stuff in this case scenario
         // if signal to controller is lost, turn off motors for safety
@@ -839,6 +909,8 @@ async fn control_loop() {
             armed = false;
         }
         CONTROL_LOOP_VALUES.signal((armed, throttle_input, [t1, t2, t3, t4]));
+
+        last_altitude = current_altitude;
     }
 }
 
